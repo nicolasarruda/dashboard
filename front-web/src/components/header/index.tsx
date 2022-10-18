f22319e7c9565590c3e8211b19cda1d74fece515
nicolasarruda <nlarruda@hotmail.com>
2022-10-18 18:28:54 -0300

    Add Header component

---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">DS Sales</h1>
+    </header>
+  );
+}
+
+export default header;
